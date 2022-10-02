a5fe7fe95d3d4870d569c9a024afb31a94d718b9
cuongtk1002 <tkcuong1002@gmail.com>
2022-10-02 12:50:58 +0700

    prime

---
+++ fh.js
@@ -0,0 +1,7 @@
+for (var i in names ) {
+    console.log(names[i]);
+}
+
+for (var i=0; i < cars.length-1; i++) {
+    console.log(cars[i]);
+}
---
+++ prime-exercises.js
@@ -0,0 +1,3 @@
+function checkSNT(number) {
+    let
+}
